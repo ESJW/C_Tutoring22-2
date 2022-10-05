c355d1373ec300401a9b054645de2997dc431487
ESJW <84553939+ESJW@users.noreply.github.com>
2022-10-06 03:05:48 +0900

    Create 11-11.c

---
+++ 11-11.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+void merge(int* A, int* B, int* C, int size)
+{
+	int i = 0, j = 0, k = 0;
+	while (i < size && j < size) {
+		if (A[i] < B[j]) {
+			C[k++] = A[i++];
+		}
+		else {
+			C[k++] = B[j++];
+		}
+	}
+
+	// 남아있는 요소들 전부 C[]로 이동
+	while (i < size || j < size) {
+		if (i < size) {	// A[]에 값이 남아있으면
+			C[k++] = A[i++];
+		}
+		if (j < size) {	// A[]에 값이 남아있으면
+			C[k++] = B[j++];
+		}
+	}
+}
+
+void array_print(int* A, int size) {
+	int i;
+	printf("[] =  ");
+	for (i = 0; i < size; i++) {
+		printf("%d ", A[i]);
+	}
+	printf("\n");
+}
+
+int main()
+{
+	int arr[4] = { 2, 5, 7, 8 };
+	int arr2[4] = { 1, 3, 4, 6 };
+	int arr3[100];
+
+	merge(arr, arr2, arr3, sizeof(arr) / sizeof(arr[0]));
+
+	printf("A");
+	array_print(arr, sizeof(arr) / sizeof(arr[0]));
+	printf("B");
+	array_print(arr2, sizeof(arr2) / sizeof(arr2[0]));
+	printf("C");
+	array_print(arr3, sizeof(arr) / sizeof(arr[0]) * 2);
+}
